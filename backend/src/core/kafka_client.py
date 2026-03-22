addc4c8616f96c2b67ba2aee87af51dcdb0bacb1
VARUN3WARE <varunrao924@gmail.com>
2026-03-23 01:21:12 +0530

    added kafka client

---
+++ backend/src/core/kafka_client.py
@@ -0,0 +1,13 @@
+"""Kafka Client"""
+
+class KafkaClient:
+    def __init__(self):
+        pass
+    
+    def produce(self, topic, message):
+        """Produce message to Kafka topic"""
+        pass
+    
+    def consume(self, topic):
+        """Consume messages from Kafka topic"""
+        pass
